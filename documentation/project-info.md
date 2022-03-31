92416abf2af93c614dcae56f574aa07d78e9d976
VNAJack <veronica.jack73@gmail.com>
2022-03-30 23:15:55 -0700

    can load a model, process img, and display results

---
+++ documentation/project-info.md
@@ -0,0 +1,14 @@
+Welcome to our Yoga Pose Classification App!
+
+:person_in_lotus_position: Using a routine with 10 Yoga poses, this app will classify your pose and help you practice Yoga at your own pace with your computer or mobile device.
+
+:point_left: To begin, please go to the `Practice Yoga` page from the menu in the sidebar. There, you have the flexibility of choosing the pose to practice with no time limits or restrictions. Once you select a pose, you can either upload an image or take a picture.
+
+:mag: After receiving the image (which is not saved or kept in any way), the app will use a CNN model to classify your Yoga pose.
+
+:muscle: Our model was trained using the dataset...
+
+:pushpin: Check out the source code on [Github](https://github.com/edouarde1/Yoga-Pose-Classification)
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,49 +1,86 @@
 import streamlit as st
+import numpy as np
+import tensorflow as tf
+import keras
+import urllib.request
+from PIL import Image
 
-# TODO: Move this function to another file to separate model and web page
-def classify(img, pose):
-    """
-    Classifies the user's Yoga pose.
+def get_md_as_string(path):
+    #url = "https://raw.githubusercontent.com/edouarde1/Yoga-Pose-Classification/main/" + path
+    #response = urllib.request.urlopen(url)
+    #return response.read().decode("utf-8")
+    with open("../documentation/" + path, 'r', encoding="utf-8") as f:
+        text = f.read()
+    return text
 
-    Args:
-        img (_type): user photo
-        pose (String): yoga pose name
 
-    Returns:
-        Boolean: True if classified correctly
-    """
-    # TODO: decide if we want pass/fail or Double for probability of classification
-    # TODO: Adjust image for model
-    # TODO: Load model - should this be done elsewhere?
-    # TODO: Classify the image
-    return True
+@st.cache(allow_output_mutation=True)
+def load_model(model_name):
+    model = keras.models.load_model("../models/" + model_name, compile=False)
+    return model
+
+def classify_pose(img):
+    # Process Image
+    test_img = Image.open(img)
+    test_img = tf.image.resize(test_img, size=[224,224], preserve_aspect_ratio=True)
+    test_img = keras.preprocessing.image.img_to_array(test_img)
+    test_img = test_img / 255.0
+    test_img = np.expand_dims(test_img, axis=0)
     
+    # Load Model and Predict
+    model = load_model("cat_dog_model.h5") # TODO: Update model name
+    results = model.predict(test_img)
+    
+    return results
+
+# TODO: Update after determining model and result format
+def display_results(results):
+    # If showing original image and processed image, use columns
+    #left_col, right_col = st.columns(2)
+    #left_col.image(img, caption="Original Image")
+    #right_col.image(proc_img, caption="Processed Image")
+    #right_col.write("Probability of correct pose: " + result_prob)
+    
+    # If only showing original with written results:
+    st.image(picture)
+    st.write("Probability of being a dog: " + str(results[0][0]))
+
 # Main Page Info
 st.title('Yoga Pose Classification App')
-st.subheader('This app lets you practice Yoga poses!')
-st.text('We used Streamlit for this app.')
-
-# TODO: Provide users more instructions
-st.text("First, choose a pose that you want to try.")
-st.text("Second, take a picture or upload an image of yourself doing that pose.")
-st.text("Finally, click `Process` to run our model.")
+st.write(" ------ ")
 
 # SIDEBAR
-st.sidebar.header('Yoga Pose Classification App')
+st.sidebar.title("Yoga Pose Classification App")
+st.sidebar.write(" ------ ")
 
-# MENU options in the sidebar
-menu = ['Yoga Pose Classification', 'About']
-choice = st.sidebar.selectbox('Menu', menu)
+sidebar_menu = ['Project Info', 'Practice Yoga', 'About Us']
+sidebar_choice = st.sidebar.selectbox('Menu', sidebar_menu)
 
-# Bottom of sidebar
-st.sidebar.info("COSC 490 Project\n11 April 2022")
+st.sidebar.write(" ------ ")
+st.sidebar.write("Last updated: 30 March 2022") #TODO: update
 
-# SIDEBAR MENU CHOICE
-if choice == menu[0]:
+# MENU = PROJECT INFO
+if sidebar_choice == sidebar_menu[0]:
+    #st.markdown("Our project's purpose...  \n"
+    #        +"Our goal...  \n"
+    #        +"\n"
+    #        +":point_left: How to use the web app...")
+    #st.markdown("View our [code on Github](https://github.com/edouarde1/Yoga-Pose-Classification)")
+    st.write(get_md_as_string("project-info.md"))
+
+# MENU = PRACTICE YOGA
+if sidebar_choice == sidebar_menu[1]:
     
-    # Add poses to the sidebar
+    # Provide users more instructions
+    with st.expander("Instructions"):
+        st.write("First, choose a pose that you want to practice.")
+        st.write("Second, take a picture or upload an image of yourself doing that pose.")
+        st.write("Finally, click `Process` to run our model.")
+    
+    # Add pose options
+    st.subheader("Choose a Yoga Pose")
     pose = ['Pose 1', 'Pose 2', 'Pose 3', 'Pose 4', 'Pose 5']
-    pose_choice = st.radio('Choose a Yoga Pose', pose)
+    pose_choice = st.radio('Select the pose you want to practice', pose)
     
     # Load images for each pose
     if pose_choice == pose[0]:
@@ -63,38 +100,35 @@ if choice == menu[0]:
         #st.image(pose_file4)
     
     # Update page subheader and text
-    st.subheader('Yoga Pose')
+    st.subheader('Your Turn')
     
+    st.info("Privacy Info: Uploaded images are never saved or stored. "
+        +"They are held temporarily in memory for processing "
+        +"and are discarded after the results are displayed. ")
+    st.warning("For best results, please upload an image of one (1) person "
+               +"doing the Yoga pose in the center of the frame.")
+
     # Give option to upload or take a picture
     src = ['Upload Image', 'Take a picture']
     src_choice = st.radio('Source', src)
     
     if src_choice == src[0]:
-        image_file = st.file_uploader(src[0], type=['jpg', 'png', 'jpeg'])
+        picture = st.file_uploader(src[0], type=['jpg', 'png', 'jpeg'])
     else:
-        image_file = st.camera_input(src[1])
+        picture = st.camera_input(src[1])
 
     # Display the user's photo
-    if image_file:
-        st.text("Your photo")
-        st.image(image_file)
-        
-    # Process the image with the model
-    if st.button("Process"):
-        # Check if we received an image  
-        if(image_file): 
-            result = classify(image_file, pose_choice)
-            if result:
-                st.success("Success message")
-            else:
-                st.text("Fail message")
-        # If no image received
-        else:
-            st.text('Please take a picture or upload a photo.')
-            
-# SIDEBAR MENU CHOICE
-if choice == menu[1]:
-    st.subheader('COSC 490 Group Project')
-    
-    # TODO: Add information about project or direct users to GitHub
-    st.text('')
+    # TODO: decide if we want to (also) show the processed image with the stick figure on top
+    if picture is not None:
+        st.success("Image ready for processing.")
+        # Process the image with the model
+        if st.button("Process"):
+            res = classify_pose(picture)
+            display_results(res)
+
+# MENU = ABOUT US
+if sidebar_choice == sidebar_menu[2]:
+    # TODO: write more
+    st.write("We are students at the University of British Columbia Okanagan Campus, "
+                 +"and this project was created for the course COSC 490: Student-Directed Seminar "
+                 +"(Topic: Advanced Machine Learning).")
