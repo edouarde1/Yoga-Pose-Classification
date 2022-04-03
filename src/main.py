a80cb35b579bbb8dbd66b43b274643930e2ff324
Khai Hung <hungluongkhai@gmail.com>
2022-04-02 17:32:44 -0700

    Add openposetoimage function in main.py

--- src/main.py
+++ src/main.py
@@ -4,42 +4,50 @@ import tensorflow as tf
 import keras
 import urllib.request
 from PIL import Image
+import cv2
+import io
+from openpose_functions import open_pose_to_image
+
 
 def get_md_as_string(path):
     url = "https://raw.githubusercontent.com/edouarde1/Yoga-Pose-Classification/main/documentation/" + path
     response = urllib.request.urlopen(url)
     return response.read().decode("utf-8")
 
+
 @st.cache(allow_output_mutation=True)
 def load_model(model_name):
     model = keras.models.load_model("../models/" + model_name, compile=False)
     return model
 
+
 def classify_pose(img):
     # Process Image
     test_img = Image.open(img)
-    test_img = tf.image.resize(test_img, size=[224,224], preserve_aspect_ratio=True)
+    test_img = tf.image.resize(test_img, size=[224, 224], preserve_aspect_ratio=True)
     test_img = keras.preprocessing.image.img_to_array(test_img)
     test_img = test_img / 255.0
     test_img = np.expand_dims(test_img, axis=0)
-    
+
     # Load Model and Predict
-    model = load_model("cat_dog_model.h5") # TODO: Update model name
+    model = load_model("cat_dog_model.h5")  # TODO: Update model name
     results = model.predict(test_img)
-    
+
     return results
+
 
 # TODO: Update after determining model and result format
 def display_results(results):
     # If showing original image and processed image, use columns
-    #left_col, right_col = st.columns(2)
-    #left_col.image(img, caption="Original Image")
-    #right_col.image(proc_img, caption="Processed Image")
-    #right_col.write("Probability of correct pose: " + result_prob)
-    
+    # left_col, right_col = st.columns(2)
+    # left_col.image(img, caption="Original Image")
+    # right_col.image(proc_img, caption="Processed Image")
+    # right_col.write("Probability of correct pose: " + result_prob)
+
     # If only showing original with written results:
-    st.image(picture)
-    st.write("Probability of being a dog: " + str(results[0][0]))
+    st.image(results)
+    # st.write("Probability of being a dog: " + str(results[0][0]))
+
 
 # Main Page Info
 st.title('Yoga Pose Classification App')
@@ -53,61 +61,61 @@ sidebar_menu = ['Project Info', 'Practice Yoga', 'About Us']
 sidebar_choice = st.sidebar.selectbox('Menu', sidebar_menu)
 
 st.sidebar.write(" ------ ")
-st.sidebar.write("Last updated: 30 March 2022") #TODO: update
+st.sidebar.write("Last updated: 30 March 2022")  # TODO: update
 
 # MENU = PROJECT INFO
 if sidebar_choice == sidebar_menu[0]:
-    #st.markdown("Our project's purpose...  \n"
+    # st.markdown("Our project's purpose...  \n"
     #        +"Our goal...  \n"
     #        +"\n"
     #        +":point_left: How to use the web app...")
-    #st.markdown("View our [code on Github](https://github.com/edouarde1/Yoga-Pose-Classification)")
+    # st.markdown("View our [code on Github](https://github.com/edouarde1/Yoga-Pose-Classification)")
     st.write(get_md_as_string("project-info.md"))
 
 # MENU = PRACTICE YOGA
 if sidebar_choice == sidebar_menu[1]:
-    
+
     # Provide users more instructions
     with st.expander("Instructions"):
         st.write("First, choose a pose that you want to practice.")
         st.write("Second, take a picture or upload an image of yourself doing that pose.")
         st.write("Finally, click `Process` to run our model.")
-    
+
     # Add pose options
     st.subheader("Choose a Yoga Pose")
     pose = ['Pose 1', 'Pose 2', 'Pose 3', 'Pose 4', 'Pose 5']
     pose_choice = st.radio('Select the pose you want to practice', pose)
-    
+
     # Load images for each pose
     if pose_choice == pose[0]:
-        st.text('Try to do '+ pose[0])
-        #st.image(pose_file0)
+        st.text('Try to do ' + pose[0])
+        # st.image(pose_file0)
     elif pose_choice == pose[1]:
-        st.text('Try to do '+ pose[1])
-        #st.image(pose_file1)
+        st.text('Try to do ' + pose[1])
+        # st.image(pose_file1)
     elif pose_choice == pose[2]:
-        st.text('Try to do '+ pose[2])
-        #st.image(pose_file2)
+        st.text('Try to do ' + pose[2])
+        # st.image(pose_file2)
     elif pose_choice == pose[3]:
-        st.text('Try to do '+ pose[3])
-        #st.image(pose_file3)
+        st.text('Try to do ' + pose[3])
+        # st.image(pose_file3)
     else:
-        st.text('Try to do '+ pose[4])
-        #st.image(pose_file4)
-    
+        st.text('Try to do ' + pose[4])
+        # st.image(pose_file4)
+
     # Update page subheader and text
     st.subheader('Your Turn')
-    
+
     st.info("Privacy Info: Uploaded images are never saved or stored. "
-        +"They are held temporarily in memory for processing "
-        +"and are discarded after the results are displayed. ")
+            + "They are held temporarily in memory for processing "
+            + "and are discarded after the results are displayed. ")
     st.warning("For best results, please upload an image of one (1) person "
-               +"doing the Yoga pose in the center of the frame.")
+               + "doing the Yoga pose in the center of the frame.")
 
     # Give option to upload or take a picture
     src = ['Upload Image', 'Take a picture']
     src_choice = st.radio('Source', src)
-    
+
     if src_choice == src[0]:
         picture = st.file_uploader(src[0], type=['jpg', 'png', 'jpeg'])
     else:
@@ -117,14 +125,27 @@ if sidebar_choice == sidebar_menu[1]:
     # TODO: decide if we want to (also) show the processed image with the stick figure on top
     if picture is not None:
         st.success("Image ready for processing.")
-        # Process the image with the model
+        st.image(picture)
+
+        # Save the image into a temporary location for processing
+        g = io.BytesIO(picture.read())  # BytesIO Object
+        temporary_location = "images/temp.jpg"
+        with open(temporary_location, 'wb') as out:  # Open temporary file as bytes
+            out.write(g.read())  # Read bytes into file
+            # close file
+            out.close()
+
+        # Process the image with openpose
         if st.button("Process"):
-            res = classify_pose(picture)
-            display_results(res)
+            # res = classify_pose(picture)
+            # Using OpenPose to process the user's image
+            processed_img = open_pose_to_image(temporary_location)
+            display_results(processed_img)
+
 
 # MENU = ABOUT US
 if sidebar_choice == sidebar_menu[2]:
     # TODO: write more
     st.write("We are students at the University of British Columbia Okanagan Campus, "
-                 +"and this project was created for the course COSC 490: Student-Directed Seminar "
-                 +"(Topic: Advanced Machine Learning).")
+             + "and this project was created for the course COSC 490: Student-Directed Seminar "
+             + "(Topic: Advanced Machine Learning).")
---
+++ src/openpose_functions.py
@@ -0,0 +1,143 @@
+import cv2
+import math
+import numpy as np
+
+
+def open_pose_to_image(img):
+
+    # We can choose different types of model here
+    # Body-25
+    protoFile = "../openpose/models/pose/body_25/pose_deploy.prototxt"
+    weightsFile = "../openpose/models/pose/body_25/pose_iter_584000.caffemodel"
+    # MPI
+    # protoFile = "../openpose/models/pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
+    # weightsFile = "../openpose/models/pose/mpi/pose_iter_160000.caffemodel"
+    # COCO
+    # protoFile = "../openpose/models/pose/coco/pose_deploy_linevec.prototxt"
+    # weightsFile = "../openpose/models/pose/coco/pose_iter_440000.caffemodel"
+    net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
+    # Read image
+    frame = cv2.imread(img)
+    height, width, channels = frame.shape
+
+    # Resize
+    width_out = 368
+    frame1 = cv2.resize(frame, (width_out, int(width_out * height / width)), cv2.INTER_AREA)
+    height1, width1, channels1 = frame1.shape
+
+    # Create black img for drawing skeleton structure
+    black_img = np.zeros((int(width_out * height / width), width_out, 3), dtype = "uint8")
+
+    # Specify the input image dimensions
+    inWidth = 368
+    inHeight = 368
+
+    # Prepare the frame to be fed to the network
+    inpBlob = cv2.dnn.blobFromImage(frame1, 1.0 / 255, (inWidth, inHeight), (0, 0, 0), swapRB=False, crop=False)
+
+    # Set the prepared object as the input blob of the network
+    net.setInput(inpBlob)
+
+    # Make Predictions and Parse Keypoints
+    output = net.forward()
+    H = output.shape[2]
+    W = output.shape[3]
+
+    # Empty list to store the detected keypoints
+    points = []
+    for i in range(15):
+        # confidence map of corresponding body's part.
+        probMap = output[0, i, :, :]
+        # Find global maxima of the probMap.
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+        # Scale the point to fit on the original image
+        x = (width1 * point[0]) / W
+        y = (height1 * point[1]) / H
+        if prob > 0:
+            cv2.circle(black_img, (int(x), int(y)), 5, (255, 0, 0), thickness=-1, lineType=cv2.FILLED)
+            #cv2.putText(black_img, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+            # Add the point to the list if the probability is greater than the threshold
+            points.append((int(x), int(y)))
+        else:
+            points.append(None)
+
+    # Draw Skeleton
+    POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,8], [8,9], [9,10], [10,11], [8,12], [12,13], [13,14]]
+    for pair in POSE_PAIRS:
+        partA = pair[0]
+        partB = pair[1]
+        if points[partA] and points[partB]:
+            # Set threshold so that long line (which result in wrong line), will be removed
+            if math.sqrt(pow(points[partA][0] - points[partB][0], 2) + pow(points[partA][1] - points[partB][1], 2)) < (150 * int(width_out * height / width)):
+                cv2.line(black_img, points[partA], points[partB], (255, 255, 0), 2)
+
+    return black_img
+
+
+# This part is for video process
+# # capture video
+# cap = cv2.VideoCapture("video.mp4")
+# fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+# out = cv2.VideoWriter('videoName.avi', fourcc, 20, (1920, 1080))
+#
+# if cap.isOpened():
+#     # # get vcap property
+#     # width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+#     # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
+#     # print(width, height)
+#
+#     # Check if video file is opened successfully
+#     if not cap.isOpened():
+#         print("Error opening video stream or file")
+#     else:
+#         width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
+#         height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float `height`
+#         print(width, height)
+#     # Read until video is completed
+#     while cap.isOpened():
+#         # Capture frame-by-frame
+#         ret, frame = cap.read()
+#         if ret == True:
+#             width_out = int(width)
+#             frame = cv2.resize(frame, (width_out, int(width_out * height / width)), cv2.INTER_AREA)
+#             # process the frame here
+#             inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (224, 224), (0, 0, 0), swapRB=False, crop=False)
+#             net.setInput(inpBlob)
+#             output = net.forward()
+#
+#             H = output.shape[2]
+#             W = output.shape[3]
+#             # Empty list to store the detected keypoints
+#             points = []
+#             for i in range(15):
+#                 # confidence map of corresponding body's part.
+#                 probMap = output[0, i, :, :]
+#                 # Find global maxima of the probMap.
+#                 minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+#                 # Scale the point to fit on the original image
+#                 x = (width * point[0]) / W
+#                 y = (height * point[1]) / H
+#                 if prob > 1:
+#                     cv2.circle(frame, (int(x), int(y)), 15, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+#                     cv2.putText(frame, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1.4, (0, 0, 255), 3,
+#                                 lineType=cv2.LINE_AA)
+#                     # Add the point to the list if the probability is greater than the threshold
+#                     points.append((int(x), int(y)))
+#                 else:
+#                     points.append(None)
+#
+#             POSE_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 14], [14, 8], [8, 9], [9, 10],
+#                           [14, 11], [11, 12], [12, 13]]
+#
+#             for pair in POSE_PAIRS:
+#                 partA = pair[0]
+#                 partB = pair[1]
+#                 if points[partA] and points[partB]:
+#                     cv2.line(frame, points[partA], points[partB], (0, 255, 0), 3)
+#
+#             out.write(frame)
+#
+#         # Break the loop
+#         else:
+#             break
